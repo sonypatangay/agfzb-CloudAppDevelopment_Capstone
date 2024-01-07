43e76417230ff7252592795ee8f6e6dbd06a731c
SonyPatangay <sony.patangay@accenture.com>
2024-01-07 18:35:03 -0500

    commit

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -24,8 +24,10 @@ def about(request):
         return render(request, 'djangoapp/about.html', context)
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
-
+def contact(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/contact.html', context)
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
