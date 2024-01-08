54eaf894b98b7ad167e633b290a017c274c12584
SonyPatangay <sony.patangay@accenture.com>
2024-01-08 11:17:33 -0500

    commit

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,20 +1,60 @@
 import requests
 import json
+import os
 # import related models here
 from requests.auth import HTTPBasicAuth
-
-
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from .models import CarDealer
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
+def get_request(url, api_key, **kwargs):
+    print(f"GET from {url}")
+    if api_key:
+        # Basic authentication GET
+        try:
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs, auth=HTTPBasicAuth('apikey', api_key))
+        except:
+            print("An error occurred while making GET request. ")
+    else:
+        # No authentication GET
+        try:
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
+        except:
+            print("An error occurred while making GET request. ")
 
-
+    # Retrieving the response status code and content
+    status_code = response.status_code
+    print(f"With status {status_code}")
+    json_data = json.loads(response.text)
+    return json_data
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
 
 
-# Create a get_dealers_from_cf method to get dealers from a cloud function
-# def get_dealers_from_cf(url, **kwargs):
+# Create a get_dealers_from_cf method to get dealers from a cloud funct
+def get_dealers_from_cf(url):
+    results = []
+    json_result = get_request(url,"13dVXn-QWMy9nxIQGmSkUTszLcYXU_1vIxVEH_pqKiV2" )
+    print(f"result got")
+    print(f"With statusaa {json_result}")
+    # Retrieve the dealer data from the response
+    dealers = json_result
+    # For each dealer in the response
+    for dealer in dealers:
+        # Get its data in `doc` object
+        dealer_doc = dealer
+        # Create a CarDealer object with values in `doc` object
+        dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                               id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                               short_name=dealer_doc["short_name"],
+                               st=dealer_doc["st"], state=dealer_doc["state"], zip=dealer_doc["zip"])
+        results.append(dealer_obj)
+
+    return results
+
 # - Call get_request() with specified arguments
 # - Parse JSON results into a CarDealer object list
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarModel
+from .restapis import get_dealers_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -87,9 +87,16 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        context = {}
+        url = "https://sonypatangay-3000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        # Get dealers from the Cloudant DB
+        context["dealerships"] = get_dealers_from_cf(url)
+
+        dealer_names = ' '.join([dealer.short_name for dealer in context["dealerships"]])
+        return HttpResponse(dealer_names)
+
+        #return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
