2d1ba072638f859c612b47a945a0ac65653d7db7
SonyPatangay <sony.patangay@accenture.com>
2024-01-09 06:33:01 -0500

    commit

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -77,7 +77,7 @@ def get_dealer_by_id(url, dealer_id):
     json_result = get_request(url, dealerId=dealer_id)
     print(f"json_here {json_result}")
     # Create a CarDealer object from response
-    dealer = json_result
+    dealer = json_result[0]
     dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"], full_name=dealer["full_name"],
                            id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
                            short_name=dealer["short_name"],
